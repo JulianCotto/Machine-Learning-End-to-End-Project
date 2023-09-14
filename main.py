dac0615aba07f2f5c4d10cca358f72640968b864
julian.cotto <julian.cotto@g.austincc.edu>
2023-09-13 23:16:14 -0500

    Completed "Get The Data Steps" Download the Data Take a Quick Look at the Data
    Image Directory & Datasets Directory created by the script.

---
+++ main.py
@@ -0,0 +1,56 @@
+####################################################################
+# Developer: Julian Cotto
+# Date: 9/13/2023
+# File Name: main.py
+# Description: This is the main running file for the project.
+####################################################################
+
+# project requirements setup
+import sys
+import matplotlib.pyplot as plt
+import sklearn
+from packaging import version
+
+from functions import load_housing_data
+from functions import save_fig
+
+assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
+assert sys.version_info >= (3, 7)
+
+print("Welcome to Machine Learning!")
+
+# load housing data csv and create directory if it doesn't exist
+housing = load_housing_data()
+
+# return top 5 rows of housing data. print statement for testing purposes
+# longitude
+# latitude
+# housing_median_age
+# total_rooms
+# total_bedrooms
+# population
+# households
+# median_income
+# median_house_value
+# ocean_proximity
+print(housing.head())
+
+# return housing data structure info.
+print(housing.info())
+
+# return value counts for ocean_proximity column
+print(housing["ocean_proximity"].value_counts())
+
+# return a description of the dataset as a whole
+print(housing.describe())
+
+# extra code – the next 5 lines define the default font sizes
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+housing.hist(bins=50, figsize=(12, 8))
+save_fig("attribute_histogram_plots")  # extra code
+plt.show()
