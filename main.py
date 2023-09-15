762194128466dc2cd93ba22896ec0cfe3fe7eaf0
julian.cotto <julian.cotto@g.austincc.edu>
2023-09-14 22:56:27 -0500

    Adding line comments to main.py. Still to add line comments to addMainMethods.py

--- main.py
+++ main.py
@@ -5,15 +5,12 @@
 # Description: This is the main running file for the project.
 ####################################################################
 import sys
-import matplotlib.pyplot as plt
 import sklearn
-from packaging import version
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-
 from loadData import load_housing_data
-from saveFigs import save_fig
+from packaging import version
+from sklearn.model_selection import train_test_split
 
 assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
 assert sys.version_info >= (3, 7)
@@ -30,6 +27,7 @@ def main() -> None:
     # see addlMainMethods.py for more methods to accomplish this #
     ##############################################################
 
+    # create a test set and train set using random sampling
     train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
     ###############################################################
@@ -37,13 +35,16 @@ def main() -> None:
     # see addlMainMethods.py for more methods to accomplish this  #
     ###############################################################
 
+    # create an income category attribute with 5 categories
     housing["income_cat"] = pd.cut(housing["median_income"],
                                    bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                    labels=[1, 2, 3, 4, 5])
 
+    # create a stratified test set and train set based on the income category
     strat_train_set, strat_test_set = train_test_split(
         housing, test_size=0.2, stratify=housing["income_cat"], random_state=42)
 
+    # drop the income_cat attribute so the data is back to its original state
     for set_ in (strat_train_set, strat_test_set):
         set_.drop("income_cat", axis=1, inplace=True)
 
